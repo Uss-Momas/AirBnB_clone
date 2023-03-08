b25fe5f53efc8cc6dbac1c119410eb96ba5fece0
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-03-08 23:42:26 +0200

    adding more features to the base model

--- models/base_model.py
+++ models/base_model.py
@@ -8,11 +8,23 @@ import uuid
 
 class BaseModel:
     """BaseModel class that contains every method that we are going to use"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Constructor of the BaseModel class"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        # print(args) - tuple
+        # print(kwargs) - dictionary with key, value
+        if len(kwargs) != 0:
+            for key in kwargs.keys():
+                print(key)
+                if key != "__class__":
+                    if key == "created_at" or key == "updated_at":
+                        # fromisoformat converte string para datetime
+                        setattr(self, key, datetime.fromisoformat(kwargs[key]))
+                    else:
+                        setattr(self, key, kwargs[key])
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """String representation of the BaseModel class"""
