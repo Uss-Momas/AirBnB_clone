a23f3d39155f28cbbc1d4e42d5e3a9d2f6f4b1cb
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-03-08 22:09:27 +0200

    made up the BaseModel class

--- models/base_model.py
+++ models/base_model.py
@@ -3,13 +3,21 @@
 defines all common attributes/methods for other classes
 """
 from datetime import datetime
-from datetime import date
-import time
+import uuid
 
 
 class BaseModel:
     """BaseModel class that contains every method that we are going to use"""
-    updated_at = datetime.now()
+    def __init__(self):
+        """Constructor of the BaseModel class"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """String representation of the BaseModel class"""
+        cls_name = type(self).__name__
+        return "[{}] ({}) {}".format(cls_name, self.id, self.__dict__)
 
     def save(self):
         """method to update public instance attribute updated_at
@@ -24,5 +32,6 @@ class BaseModel:
         """
         dictionary = self.__dict__
         dictionary["__class__"] = type(self).__name__
+        dictionary["created_at"] = self.created_at.isoformat()
         dictionary["updated_at"] = self.updated_at.isoformat()
         return dictionary
