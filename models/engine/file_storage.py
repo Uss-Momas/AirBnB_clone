be037c9a984f394af3c7a1f640e534adf0dc0c54
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-03-09 23:30:07 +0200

    new stuffs

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """File Storage Module"""
+import json
 
 
 class FileStorage:
@@ -9,19 +10,22 @@ class FileStorage:
 
     def __init__(self):
         """Constructor"""
-        self.__file_path = ""
+        self.__file_path = "../../file.json"
         self.__objects = {}
-        ...
 
     def all(self):
         """returns the dictionary __objects"""
         return self.__objects
 
-    def new(self):
+    def new(self, obj):
         """Sets in __objects the obj with key"""
+        key = type(obj) + "." + obj.id
+        self.__objects[key] = obj
 
     def save(self):
         """Serializes __objects to the JSON file"""
+        with open(self.__file_path, encoding="utf-8") as f:
+            json.dumps()
 
     def reload(self):
         """deserializes the JSON file to __objects"""
