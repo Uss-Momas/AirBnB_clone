2b32e3454daf02f55b708c8697fd07bd43e451d7
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-03-09 21:45:57 +0200

    Testing the class manually

--- test_base_model_dict.py
+++ test_base_model_dict.py
@@ -7,10 +7,11 @@ bm = BaseModel()
 bm.name = "My first Model"
 bm.my_number = 98
 
+print(bm.id)
 print(bm)
-bm.save()
-print(bm)
+print(type(bm.created_at))
 
+print("----")
 my_model_json = bm.to_dict()
 print(my_model_json)
 
@@ -21,4 +22,15 @@ for key in my_model_json.keys():
 print("--")
 
 new_model = BaseModel(**my_model_json)
+print(new_model.id)
 print(new_model)
+print(type(new_model.created_at))
+
+
+print("=----")
+print(bm is new_model)
+
+print("---")
+
+base = BaseModel(None, None)
+print(base)
