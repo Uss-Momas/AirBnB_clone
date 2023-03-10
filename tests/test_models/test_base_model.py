63dcc4092144f8f90fc7bec758d4ac50e4f55837
Amâncio Chiluvane <109949333+amanciochiluvane@users.noreply.github.com>
2023-03-10 14:57:46 +0200

    Updated teste_base_model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,7 +1,25 @@
 #!/usr/bin/python3
-from base_model import BaseModel
+"""
+Testing the base_model
+"""
+from models.base_model import BaseModel
+import datetime
+import unittest
 
-my_model = BaseModel()
-my_model.name = "My First Model"
-my_model.my_number = 89
-print(my_model)
+
+class TestBaseModel(unittest.TestCase):
+    """The test class"""
+
+    def test_object_type(self):
+        bm = BaseModel()
+        self.assertIsInstance(bm.created_at, datetime.datetime)
+        self.assertIsInstance(bm.updated_at, datetime.datetime)
+        self.assertIsInstance(bm.id, str)
+
+    def test_to_dict(self):
+        bm = BaseModel()
+        dct = bm.to_dict()
+        self.assertIsInstance(dct, dict)
+
+    def test_initialize_fake_args(self):
+        ...
