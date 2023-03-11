84ba2dbc887ed1b5d8f91aa953a657380ff0b532
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-03-11 21:19:50 +0200

    create command implemented to create BaseModel

--- console.py
+++ console.py
@@ -1,12 +1,29 @@
 #!/usr/bin/python3
 """Console Module"""
 import cmd
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
     """Console Class"""
 
     prompt = "(hbnb)"
+
+    def do_create(self, classname):
+        """create: creates a new instance of the BaseModel,
+        save it to JSON file and prints the id
+        usage: create class_name
+        """
+        class_names_list = ["BaseModel"]
+        if classname:
+            if classname in class_names_list:
+                new_model = BaseModel()
+                new_model.save()
+                print(new_model.id)
+            else:
+                print("** class doesn't exist **")
+        else:
+            print("** class name is missing **")
 
     def do_quit(self, line):
         """Quit: command to exit the program"""
