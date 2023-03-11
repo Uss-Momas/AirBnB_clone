b2963b1208d01ab9cda1d4122fe92a70fec52bfd
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-03-11 22:38:51 +0200

    Printing all the information of the file.json in the console with all method

--- console.py
+++ console.py
@@ -12,6 +12,30 @@ class HBNBCommand(cmd.Cmd):
     class_names_list = ["BaseModel"]
 
     # general purpose methods of the program
+    def do_all(self, line):
+        """all: Prints all string representation of all instances or
+        not on class name
+        usage: all
+        or usage: all class_name
+        """
+        str_list_cls_name = []
+        dict_objects = storage.all()
+        if line:
+            if line in HBNBCommand.class_names_list:
+                for key, obj in dict_objects.items():
+                    cls_name = key.split(".")[0]
+                    if cls_name in HBNBCommand.class_names_list:
+                        str_list_cls_name.append(str(obj))
+                print(str_list_cls_name)
+            else:
+                print("** class doesn't exist **")
+        else:
+            for key, obj in dict_objects.items():
+                cls_name = key.split(".")[0]
+                if cls_name in HBNBCommand.class_names_list:
+                    str_list_cls_name.append(str(obj))
+            print(str_list_cls_name)
+
     def do_create(self, classname):
         """create: creates a new instance of the BaseModel,
         save it to JSON file and prints the id
@@ -26,6 +50,37 @@ class HBNBCommand(cmd.Cmd):
         else:
             print("** class name is missing **")
 
+    def do_destroy(self, line):
+        """detroy: Deletes and instance based on a class name and id
+        usage: destroy class_name id
+        """
+        if line:
+            class_name = line.split(" ")[0]
+            if class_name in HBNBCommand.class_names_list:
+                length = len(line.split(" "))
+                if length >= 2:
+                    match = False
+                    dictionary = storage.all()
+                    id = line.split(" ")[1]
+                    for key, value in dictionary.items():
+                        id_key = key.split(".")[1]
+                        if id == id_key:
+                            match = True
+                            key = key
+                            break
+                    if match:
+                        dictionary.pop(key)
+                        storage.save()
+                    else:
+                        print("** no instance found **")
+                else:
+                    print("** instance id missing **")
+
+            else:
+                print("** class doesn't exist **")
+        else:
+            print("** class name missing **")
+
     def do_show(self, line):
         """show: prints the string representation of a instance based on
         the class name and id.
@@ -37,8 +92,8 @@ class HBNBCommand(cmd.Cmd):
                 if len(line.split(" ")) >= 2:
                     match = False
                     id = line.split(" ")[1]
-                    dict = storage.all()
-                    for key, value in dict.items():
+                    dictionary = storage.all()
+                    for key, value in dictionary.items():
                         id_key = key.split(".")[1]
                         if id == id_key:
                             match = True
